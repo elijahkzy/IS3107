5c3426f2abce5453df9d391959dc44083946ef54
estella <77688520+eseutella@users.noreply.github.com>
2023-04-06 03:26:47 +0800

    transform stock data

--- Setup BigQuery.py
+++ Setup BigQuery.py
@@ -2,15 +2,18 @@ from google.cloud import bigquery
 import os
 import json
 
+# Change to your key
+key = 'key.json'
+
 #Get Project ID
-openfile=open('C:/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/test-proj-378801-e260b3ef768e.json')
-jsondata=json.load(openfile)
+openfile = open(key)
+jsondata = json.load(openfile)
 openfile.close()
 project_id = jsondata['project_id']
 
 # Construct a BigQuery client object.
-credentials_path = 'C:/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/test-proj-378801-e260b3ef768e.json'
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= credentials_path
+credentials_path = key
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
 client = bigquery.Client()
 
 # TODO(developer): Set dataset_id to the ID of the dataset to create.
@@ -20,7 +23,7 @@ def setupDataset():
     '''
     Sets up the dataset
     '''
-    dataset = ["yfinance_data_raw", "stock_info", 'twitter_data'] #list of datasets to make 
+    dataset = ["yfinance_data_raw", "yfinance_data", "stock_info", "twitter_data_raw", "twitter_data"] #list of datasets to make 
 
     for d in dataset:
         # Construct a full Dataset object to send to the API.
@@ -39,6 +42,26 @@ def setupTable():
     '''
     Sets up the respective tables
     '''
+
+    #Setup YFinance Table
+    schema = [
+        bigquery.SchemaField("Date", "TIMESTAMP"),
+        bigquery.SchemaField("Ticker", "STRING"),
+        bigquery.SchemaField("Open", "FLOAT"),
+        bigquery.SchemaField("High", "FLOAT"),
+        bigquery.SchemaField("Low", "FLOAT"),
+        bigquery.SchemaField("Close", "FLOAT"),
+        bigquery.SchemaField("Adj_Close", "FLOAT"),
+        bigquery.SchemaField("Volume", "INTEGER"),
+        bigquery.SchemaField("MA_5days", "FLOAT"),
+        bigquery.SchemaField("Signal", "STRING")
+    ]
+    table = bigquery.Table(project_id + "." + "yfinance_data.stock_info", schema=schema)
+    table = client.create_table(table)  # Make an API request.
+    print(
+        "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+    )
+
     #Setup Dividend Table
     schema = [
         bigquery.SchemaField("Stock", "STRING"),
--- stock_info_dag.py
+++ stock_info_dag.py
@@ -1,11 +1,12 @@
-import datetime
-from google.cloud import bigquery 
-import pandas as pd
+from datetime import datetime, timedelta 
 import yfinance as yf
+import pandas as pd
+import pandas_gbq
+from google.cloud import bigquery
 import os
 import json
 
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import os
 import csv
@@ -13,15 +14,11 @@ import logging
 
 from airflow import DAG
 
+# Change to your key
+key = '/mnt/c/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/test-proj-378801-e260b3ef768e.json'
+# key = '/mnt/c/Users/Estella Lee Jie Yi/OneDrive - National University of Singapore/Desktop/NUS/Y3S2/IS3107/project/key.json'
 
-from datetime import datetime, timedelta  
-import datetime as dt
-import numpy as np
-import pandas as pd
-
-
-
-def stockdata_extract(ti):
+def financials_extract(ti):
     '''
     Extract stock information in STI Components for past five days
     Gets the stock data using Yahoo Finance API in Pandas Dataframe and push as JSON
@@ -30,10 +27,9 @@ def stockdata_extract(ti):
     Output: None
     '''
     #STI Index
-    tickers = ['C6L.SI','D05.SI','C52.SI','G13.SI','C38U.SI',
-            'U11.SI','Y92.SI','N2IU.SI','S58.SI','F34.SI',
-            'M44U.SI','Z74.SI','C09.SI','BS6.SI','V03.SI']
-    stock_dat = yf.download(tickers,period="2d",actions=False,group_by=tickers)
+    tickers = ['YF8.SI','BS6.SI', 'J36.SI', 'O39.SI', 'D05.SI', 'U11.SI', 'C09.SI', '9CI.SI', 'C07.SI', 'A17U.SI', 'Z74.SI', 'ME8U.SI', 'AJBU.SI',	
+               'C38U.SI', 'M44U.SI', 'U14.SI', 'V03.SI', 'D01.SI', 'H78.SI', 'BN4.SI', 'BUOU.SI', 'Y92.SI', 'C52.SI', 'S63.SI',	'G13.SI', 'EMI.SI',
+               'N2IU.SI', 'F34.SI', 'U96.SI', 'S58.SI']
     i = 0
     df = pd.DataFrame()
 
@@ -45,32 +41,76 @@ def stockdata_extract(ti):
         prices.insert(loc = 1, column = 'Ticker', value = ticker)
         prices = prices.rename({'Adj Close': 'Adj_Close'}, axis=1)
         df = pd.concat([df, prices],ignore_index = True)
-        i+= 1
+        i += 1
 
+    print(df)
     stock_info_daily = df.to_json(orient='records')
     ti.xcom_push(key='stock_info_daily', value=stock_info_daily)
 
-def stockdata_upload(ti):
-    '''push the raw stockdata into google bigquery dataset yfinance_data_raw
+def financials_stage(ti):
     '''
-    stock_info_daily = ti.xcom_pull(key='stock_info_daily', task_ids=['get_stock_info'])[0]
+    Push the raw stockdata into google bigquery dataset yfinance_data_raw
+    '''
+    stock_info_daily = ti.xcom_pull(key='stock_info_daily', task_ids=['financials_extract'])[0]
     df = pd.DataFrame(eval(stock_info_daily))
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms') # Convert date from json to date format
 
-    openfile=open('/mnt/c/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/test-proj-378801-e260b3ef768e.json')
-    jsondata=json.load(openfile)
+    openfile = open(key)
+    jsondata = json.load(openfile)
     openfile.close()
-    project_id = jsondata['project_id']
 
     # Construct a BigQuery client object.
-    credentials_path = '/mnt/c/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/test-proj-378801-e260b3ef768e.json'
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= credentials_path
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
     client = bigquery.Client()
     
     project_id = jsondata['project_id']
-    staging_table_id = project_id + ".yfinance_data_raw.stock_info"
-    job = client.load_table_from_dataframe(df, staging_table_id)
-    job.result()
+    # staging_table_id = project_id + ".yfinance_data_raw.stock_info"
+    # job = client.load_table_from_dataframe(df, staging_table_id)
+    # job.result()
+    staging_table_id = "yfinance_data_raw.stock_info"
+    pandas_gbq.to_gbq(df, destination_table=staging_table_id, 
+                      project_id=project_id,
+                      if_exists='append')
 
+def financials_transform():
+    '''
+    Load Stock Data from Staging Table to Main Table
+    Derive Moving-Average(5) and Signal columns using SQL, and pass Close, Moving-Average(5) and Signal to Main Tables
+    '''
+    #Get Project ID
+    openfile = open(key)
+    jsondata = json.load(openfile)
+    openfile.close()
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".yfinance_data_raw.stock_info"
+    actual_table_id = project_id + ".yfinance_data.stock_info"
+    
+    #Connect To Bigquery
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    #Load Data from Staging table to Main table
+    query = f"""
+        INSERT INTO `{actual_table_id}`
+        SELECT *
+        FROM (SELECT *, AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS MA_5day,
+        CASE
+        WHEN ((Close - AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW)) > 0.1) or (AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) - Close) > 0.1 THEN 'Neutral'
+        WHEN Close > AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Buy'
+        WHEN Close < AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Sell'
+        else 'Neutral'
+        END AS Signal,
+        FROM
+        `{staging_table_id}` as p
+        ) T
+        where CAST(DATE as Date) = CURRENT_DATE();
+
+        TRUNCATE TABLE `{staging_table_id}`;
+    """
+    query_job = client.query(query)
+    print('Successfully loaded stock prices')
 
 default_args = {
      'owner': 'airflow',
@@ -81,25 +121,31 @@ default_args = {
      'retries': 1
     }
 
-with DAG( 'stocks_info_dag',
-            default_args=default_args,
-            description='Collect Stock Info For Analysis',
-            catchup=False, 
-            start_date= datetime(2020, 12, 23), 
-            schedule_interval= None # to change to 0 0 * * * (daily)
-  
-          )  as dag:
+with DAG(
+    'stocks_info_dag',
+    default_args=default_args,
+    description='Collect Stock Info For Analysis',
+    catchup=False, 
+    start_date=datetime(2020, 12, 23), 
+    schedule_interval='@daily' #None # to change to 0 0 * * * (daily)
+) as dag:
     
-    get_daily_stock_info = PythonOperator(
-        task_id='get_stock_info',
+    financialsExtract = PythonOperator(
+        task_id='financials_extract',
         provide_context=True,
-        python_callable=stockdata_extract
+        python_callable=financials_extract
     )
 
-    push_daily_stock_info = PythonOperator(
-        task_id='push_stock_info',
+    financialsStage = PythonOperator(
+        task_id='financials_stage',
         provide_context=True,
-        python_callable=stockdata_upload
+        python_callable=financials_stage
     )
 
-get_daily_stock_info >> push_daily_stock_info
+    financialsTransform = PythonOperator(
+        task_id='financials_transform',
+        provide_context=True,
+        python_callable=financials_transform
+    )
+
+financialsExtract >> financialsStage >> financialsTransform
