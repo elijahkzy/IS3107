dd39e4c61f3cee76d9f46b700d82e71aa8cd94a0
HsinZhengWei <91802401+HsinZhengWei@users.noreply.github.com>
2023-04-19 03:03:42 +0800

    Merged both files together
    Only appending 1 day worth of scraped tweets daily

--- twitter_data_dag.py
+++ twitter_data_dag.py
@@ -1,32 +1,27 @@
-import datetime
 from google.cloud import bigquery 
 import pandas as pd
 import yfinance as yf
 import os
 import json
 import snscrape.modules.twitter as sntwitter
-from datetime import datetime, timedelta  
-
+from datetime import timedelta  
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-import csv
-import logging
-import io
-
 from airflow import DAG
 import tweepy
-from datetime import datetime, timedelta, date
-
+from datetime import datetime, timedelta
 from tweepy import OAuthHandler
-from timeit import default_timer as timer
-
-import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 # Change to your key
 # key = '/mnt/c/Users/hsinz/Desktop/nus/Y3S2/IS3107/Proj/privateKey.json'
 key = '/mnt/c/Users/hsinz/Desktop/nus/Y3S2/IS3107/Proj/sharedkey.json'
 
 def tweetdata_extract(ti):
+    #scrape 1 day of tweets up to 100 tweets based on tickers
     tickers=[
         ('d05.SI', 'dbs'),
         ('SE', 'garena'),
@@ -45,62 +40,80 @@ def tweetdata_extract(ti):
         ('S63.SI', 'st engineering'),
         ('BN4.SI', 'keppel')
     ]
-
-    # Create an empty list to store the tweets
-    tweets = []
-    for tup in tickers:
-        keyword = f'{tup[1]}' #can use OR to specify other things #hashtag also can
-        # start_date = date(2023, 4, 1)
-        # end_date = date(2023, 4, 7)
-        end_date = date.today()
-        start_date = end_date - timedelta(days = 7)
-        user = ''
-
-        # Create a list of dates between start_date and end_date
-        delta = timedelta(days=1)
-        date_range = pd.date_range(start_date, end_date, freq='D').tolist()
-
-
-        #limit of tweets per day
-        limit = 10
-
-        # Loop over each day in the date range and retrieve X tweets per day
-        #if count == limit or when the loop ends
-        for i in range(len(date_range)):
-            day = date_range[i].strftime('%Y-%m-%d')
-            dayNext = (date_range[i] + timedelta(days = 1)).strftime('%Y-%m-%d')
-            query = f'{keyword} from:{user} since:{day} until:{dayNext} lang:en'
-            count = 0
-            for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-                if (count == limit):
-                    break
-                tweets.append([keyword, tup[0], tweet.date, tweet.content, tweet.likeCount])
-                count+= 1
-            print(f'{keyword}_{day}: {count} total tweets retrieved')
-
-    # Convert the list of tweets to a Pandas DataFrame
-    df = pd.DataFrame(tweets, columns=['Ticker_Name', 'Ticker', 'Datetime', 'Texts', 'Likecount'])
     
+    consumer_key = 'omhSwvUaRXdSh4fkq1Q4VhU36'
+    consumer_secret = 'a4uNlAvLxQ1AE48sP9bdLTDKP8IKxONdLZXff7Ue6foSliY3s5'
+    access_token = '1278982307303419908-RewTtBb6Uj2ySXfAr6G5o8NjDb8IRm'
+    access_secret = '1oY9ZCHHXWykuyctbtGKPposnfpZKIJpLQxUSC3Xylhwa'
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    client = tweepy.Client(
+        bearer_token='AAAAAAAAAAAAAAAAAAAAAGzylgEAAAAA5UESpwQdxCu2wO5e8x8A0looTk0%3DCIIzaw5kytSTDaW6zWNvs7b1MsshuYE9gyA06vy0dkNwqk07gB',
+        return_type=dict)
+
+    tickerName = []
+    author_id = []
+    texts = []
+    created_dates = []
+    tickerCode = []
+    likeCount = []
+   
+    # Get the current UTC date and time
+    utc_now = datetime.utcnow()
+
+    # Set the time to 00:00:00
+    utc_today = datetime.combine(utc_now.date(), datetime.min.time())
+
+    # Format the datetime object to a string
+    formatted_utc_today = utc_today.strftime('%Y-%m-%dT%H:%M:%SZ')
+    print(formatted_utc_today)
+
+    for ticker in tickers:
+        print(ticker[1])
+        keyword = ticker[1] if ticker[1] != 'grab' else '#grab'
+
+        tweets = client.search_recent_tweets(query= keyword, max_results=100,
+            start_time = formatted_utc_today,
+            tweet_fields = ['author_id','created_at','text','source','lang','geo', 'public_metrics'],
+            user_fields = ['name','username','location','verified'],
+            expansions = ['geo.place_id', 'author_id'],
+            place_fields = ['country','country_code'])
+        try:
+            for tweet in tweets['data']:
+                if tweet['lang'] == 'en':
+                    tickerName.append(keyword if keyword != '#grab' else 'grab')
+                    texts.append(tweet['text'])
+                    created_dates.append(tweet['created_at'])
+                    author_id.append(tweet['author_id'])
+                    tickerCode.append(ticker[0])
+                    likeCount.append(tweet['public_metrics']['like_count'])
+        except:
+            tickerName.append('')
+            texts.append('')
+            created_dates.append('')
+            author_id.append('')
+            tickerCode.append(ticker[0])
+            likeCount.append('')
+
+    df = pd.DataFrame({'Ticker_Name': tickerName,
+                       'Ticker': tickerCode,
+                        'Texts':texts, 
+                        'Likecount': likeCount,
+                        'Datetime': created_dates})
     twitter_data = df.to_json(orient='records')
-    # print(twitter_data)  
     ti.xcom_push(key='twitter_data', value=twitter_data)
 
 def tweetdata_upload(ti):
-    '''push the twitter data into bigquery 
+    '''push 1 day of twitter data into bigquery 
     '''
-    twitter_data = ti.xcom_pull(key='twitter_data', task_ids=['get_twitter_data'])[0]
+    twitter_data = ti.xcom_pull(key='twitter_data', task_ids=['get_twitter_data'])
+    json_str = ''.join(twitter_data)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+
     openfile=open(key)
     jsondata=json.load(openfile)
     openfile.close()
-    df = pd.read_json(io.StringIO(twitter_data), encoding='utf-8')
-
-    df['Texts'] = df['Texts'].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8'))
-    df['Ticker_Name'] = df['Ticker_Name'].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8'))
-    # df['Datetime'] = df['Datetime'].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8'))
-    df['Ticker'] = df['Ticker'].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8'))
-    df['Likecount'] = df['Likecount'].apply(lambda x: x.encode('utf-8', errors='replace').decode('utf-8'))
-
-    print(df)
 
     # Construct a BigQuery client object.
     credentials_path = key
@@ -108,14 +121,135 @@ def tweetdata_upload(ti):
     client = bigquery.Client()
     
     project_id = jsondata['project_id']
-    staging_table_id = project_id + ".twitter_data_raw.stock_info"
+    staging_table_id = project_id + ".twitter_data.stock_info"
+    job_config = bigquery.LoadJobConfig(
+        encoding='UTF-8',
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+    )
+    job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
+    job.result()
+    print(f'Appended {len(df)} rows of date')
+
+def get_staging_data(ti):
+    # pull data from staging area of twitter table in bigquery
+
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    openfile = open(key)
+    jsondata = json.load(openfile)
+
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".twitter_data.stock_info"
+
+    table = client.get_table(staging_table_id)
+    df = client.list_rows(table).to_dataframe()
+    twitter_staging_data = df.to_json(orient='records')
+
+    print(f'Successfully loaded twitterdata with {len(df)} rows')
+    ti.xcom_push(key = 'twitter_staging_data', value = twitter_staging_data)
+
+def clean_tweet(tweet):
+        # method for cleaning tweets used in clean_twitter_data dag
+
+        tweet = re.sub(r'http\S+', '', tweet)
+        # Remove mentions
+        tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
+        # Remove hashtags
+        tweet = re.sub(r'#', '', tweet)
+        # Remove non-alphabetic characters
+        tweet = re.sub(r'[^a-zA-Z\s]', '', tweet)
+        # Convert to lowercase
+        tweet = tweet.lower()
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        words = tweet.split()
+        filtered_words = [word for word in words if word not in stop_words]
+        return ' '.join(filtered_words)
+
+def splitScore(df):
+    df['Negative'] = df['Score'].apply(lambda x: x.get('neg'))
+    df['Neutral'] = df['Score'].apply(lambda x: x.get('neu'))
+    df['Positive'] = df['Score'].apply(lambda x: x.get('pos'))
+    df['Compound'] = df['Score'].apply(lambda x: x.get('compound'))
+    return df
+
+def weighted_avg(df):
+    weights = df['Likecount'] + 1
+    return (df['Compound'] * weights).sum() / weights.sum()
+
+def clean_twitter_data(ti):
+    # cleans the data, removing hashtags, stop words etc.
+
+    # twitter_data_str = ti.xcom_pull(key='twitter_data', task_ids=['get_twitter_data'])
+    twitter_data_str = ti.xcom_pull(key='twitter_staging_data')
+    json_str = ''.join(twitter_data_str)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+
+    df['Texts_cleaned'] = df['Texts'].apply(lambda x: clean_tweet(x))
+    twitter_data_cleaned = df.to_json(orient = 'records')
+    ti.xcom_push(key = 'twitter_data_cleaned', value = twitter_data_cleaned)
+    print('Sucessfully cleaned data')
+
+def process_twitter_data(ti):
+    # NLP on the cleaned data using sentiment analysis 
+
+    twitter_data_str = ti.xcom_pull(key='twitter_data_cleaned')
+    json_str = ''.join(twitter_data_str)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+    
+    sia = SentimentIntensityAnalyzer()
+    df['Score'] = df['Texts_cleaned'].apply(lambda x: sia.polarity_scores(x))
+    df = splitScore(df)
+    df['Date'] = df['Datetime'].apply(lambda x: x.date())
+    df.to_json(orient='records')
+
+    aggregated_data = df.groupby(['Ticker_Name', 'Ticker', 'Date'], as_index = False).apply(weighted_avg)
+    aggregated_data.rename(columns = {None: 'WeightedCompoundScore'}, inplace = True)
+
+    twitter_data_processed = df.to_json(orient = 'records')
+    aggregated_data_processed = aggregated_data.to_json(orient = 'records')
+    ti.xcom_push(key = 'twitter_data_processed', value = twitter_data_processed)
+    ti.xcom_push(key = 'aggregated_data_processed', value = aggregated_data_processed)
+
+    print('Successfully processed data')
+
+def tweet_processed_data_upload(ti):
+    #  uploads data into bigquery
+
+    twitter_data_processed = ti.xcom_pull(key='twitter_data_processed')
+    json_str = ''.join(twitter_data_processed)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+
+    aggregated_data_processed = ti.xcom_pull(key = 'aggregated_data_processed')
+    json_str_agg = ''.join(aggregated_data_processed)
+    df_aggregated = pd.read_json(json_str_agg, encoding='utf-8', orient = 'records')
+
+    openfile=open(key)
+    jsondata=json.load(openfile)
+    openfile.close()
+
+    # Construct a BigQuery client object.
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".twitter_data.stock_processed"
     job_config = bigquery.LoadJobConfig(
         encoding='UTF-8',
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
     )
+
     job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
+    aggregated_table_id = project_id + '.twitter_data.stock_aggregated'
+    job = client.load_table_from_dataframe(df_aggregated, aggregated_table_id, job_config=job_config)
+
     job.result()
 
+    print(f'Pushed data into big query table {staging_table_id}')
+    print(f'Pushed aggregated data into big query table {aggregated_table_id}')
 
 default_args = {
      'owner': 'airflow',
@@ -146,4 +280,28 @@ with DAG(
         python_callable=tweetdata_upload
     )
 
-get_twitter_data >> push_twitter_data
+    get_staging_data = PythonOperator(
+        task_id='get_staging_data',
+        provide_context=True,
+        python_callable=get_staging_data
+    )
+
+    clean_twitter_data = PythonOperator(
+        task_id='clean_twitter_data',
+        provide_context=True,
+        python_callable=clean_twitter_data
+    )
+
+    process_twitter_data = PythonOperator(
+        task_id='process_twitter_data',
+        provide_context=True,
+        python_callable=process_twitter_data
+    )
+
+    push_processed_twitter_data = PythonOperator(
+        task_id='push_processed_twitter_data',
+        provide_context=True,
+        python_callable=tweet_processed_data_upload
+    )
+
+get_twitter_data >> push_twitter_data >> get_staging_data >> clean_twitter_data >> process_twitter_data >> push_processed_twitter_data
