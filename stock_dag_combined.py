7b5dff42a8eb2938d6409b75c7ef076c1233695a
Marklim99 <77767460+Marklim99@users.noreply.github.com>
2023-04-20 18:06:23 +0800

    final dags

---
+++ stock_dag_combined.py
@@ -0,0 +1,270 @@
+from datetime import datetime, timedelta 
+import yfinance as yf
+import pandas as pd
+import pandas_gbq
+from google.cloud import bigquery
+import os
+import json
+from sqlalchemy import create_engine
+
+from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+import os
+import csv
+import logging
+import smtplib
+from airflow import DAG
+
+# Change to your key
+key = '/mnt/c/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/key.json'
+# local postgres db credentials 
+username = "postgres"
+password = "postgres"
+host = "localhost"
+port = "5432"
+database = "is3107"
+
+# create database engine
+engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
+
+
+def financials_extract(ti):
+    '''
+    Extract stock information in STI Components for past five days
+    Gets the stock data using Yahoo Finance API in Pandas Dataframe and push as JSON
+    Input: None
+    Output: None
+    '''
+    #STI Index
+    tickers =  ['D05.SI', 'SE', 'O39.SI', 'U11.SI' ,'Z74.SI', 'F34.SI', 'C6L.SI', 'GRAB', 'G13.SI', 'C38U.SI','G07.SI', 'C07.SI', 'A17U.SI', 'S63.SI', 'BN4.SI']
+    i = 0
+    df = pd.DataFrame()
+
+    for ticker in tickers:
+        prices = yf.download(ticker, period='1d').iloc[: , :6].dropna(axis=0, how='any')
+        prices = prices.loc[~prices.index.duplicated(keep='last')]
+        prices = prices.reset_index()
+
+        prices.insert(loc = 1, column = 'Ticker', value = ticker)
+        prices = prices.rename({'Adj Close': 'Adj_Close'}, axis=1)
+        df = pd.concat([df, prices],ignore_index = True)
+        i += 1
+
+    print(df)
+    stock_info_daily = df.to_json(orient='records')
+    ti.xcom_push(key='stock_info_daily', value=stock_info_daily)
+
+def financials_stage(ti):
+    '''
+    Push the raw stockdata into google bigquery dataset yfinance_data_raw
+    '''
+    stock_info_daily = ti.xcom_pull(key='stock_info_daily', task_ids=['financials_extract'])[0]
+    df = pd.DataFrame(eval(stock_info_daily))
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms') # Convert date from json to date format
+
+    openfile = open(key)
+    jsondata = json.load(openfile)
+    openfile.close()
+
+    # Construct a BigQuery client object.
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+    
+    project_id = jsondata['project_id']
+    # staging_table_id = project_id + ".yfinance_data_raw.stock_info"
+    # job = client.load_table_from_dataframe(df, staging_table_id)
+    # job.result()
+    staging_table_id = "yfinance_data.staging_stock_info"
+    pandas_gbq.to_gbq(df, destination_table=staging_table_id, 
+                      project_id=project_id,
+                      if_exists='append')
+    df.to_sql("raw_stock_data", con=engine, if_exists="append", index=False)
+
+
+def financials_transform():
+    '''
+    Load Stock Data from Staging Table to Main Table
+    Derive Moving-Average(5) and Signal columns using SQL, and pass Close, Moving-Average(5) and Signal to Main Tables
+    '''
+    #Get Project ID
+    openfile = open(key)
+    jsondata = json.load(openfile)
+    openfile.close()
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".yfinance_data.staging_stock_info"
+    actual_table_id = project_id + ".yfinance_data.stock_info"
+    
+    #Connect To Bigquery
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    #Load Data from Staging table to Main table
+    query =  f"""
+        INSERT INTO `{actual_table_id}`
+        SELECT *
+        FROM (SELECT *, AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS MA_5day,
+        CASE
+        WHEN ((Close - AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW)) > 0.1) or (AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) - Close) > 0.1 THEN 'Neutral'
+        WHEN Close > AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Buy'
+        WHEN Close < AVG(CAST(Close AS float64)) OVER (PARTITION BY p.Ticker ORDER BY Date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Sell'
+        else 'Neutral'
+        END AS Signal,
+        FROM
+        `{staging_table_id}` as p
+        ) T
+        where CAST(DATE as Date) = CURRENT_DATE();
+        TRUNCATE TABLE `{staging_table_id}`;
+    """
+    query_job = client.query(query)
+    
+    if query_job.state == 'DONE':
+        if query_job.error_result:
+            print(f"Query job failed with error: {query_job.error_result['message']}")
+        else:
+            print("Query job succeeded!")
+    else:
+            print("Query job is still running.")
+
+    query_local = """CREATE TABLE IF NOT EXISTS processed_stock_data AS
+        SELECT *
+        FROM raw_stock_data 
+        WHERE 1 = 0;
+
+        alter table processed_stock_data
+        add column if not exists"ma_5day" FLOAT ,
+        add column if not exists "signal" Varchar(10);
+
+        INSERT INTO processed_stock_data  
+        SELECT *,
+        AVG(p."Close") OVER (PARTITION BY p."Ticker" ORDER BY p."Date"  ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS MA_5day,
+        CASE
+        WHEN ((p."Close" - AVG(p."Close") OVER (PARTITION BY p."Ticker" ORDER BY p."Date" ROWS BETWEEN 4 PRECEDING AND CURRENT ROW)) > 0.1) or (AVG(p."Close") OVER (PARTITION BY p."Ticker" ORDER BY p."Date" ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) - p."Close") > 0.1 THEN 'Neutral'
+        WHEN p."Close" > AVG(p."Close") OVER (PARTITION BY p."Ticker" ORDER BY p."Date" ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Buy'
+        WHEN p."Close" < AVG(p."Close") OVER (PARTITION BY p."Ticker" ORDER BY p."Date" ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) THEN 'Sell'
+        else 'Neutral'
+        END AS Signal
+        FROM raw_stock_data as p
+        WHERE CAST(p."Date" as Date) = CURRENT_DATE;
+
+        INSERT INTO processed_stock_data SELECT * FROM  raw_stock_data;
+        TRUNCATE raw_stock_data
+        """
+
+    with engine.connect() as connection:
+        connection.execute(query_local)
+
+def financials_load():
+    '''
+    Load Stock Data to combined data 
+    Combined with twitter data
+    '''
+    #Get Project ID
+    openfile = open(key)
+    jsondata = json.load(openfile)
+    openfile.close()
+    project_id = jsondata['project_id']
+    finance_table_id = project_id + ".yfinance_data.stock_info"
+    twitter_table_id = project_id + ".twitter_data.stock_aggregated"
+    load_table_id = project_id + ".combined_data.stock_info"
+    
+    #Connect To Bigquery
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    #Joining finance table and twitter scores table per day
+    query = f"""
+        INSERT INTO `{load_table_id}`
+        SELECT 
+            A.Date,
+            A.Ticker,
+            A.Open,
+            A.High,
+            A.Low,
+            A.Close,
+            A.Adj_Close,
+            A.Volume,
+            A.MA_5days,
+            A.Signal,
+            B.WeightedCompoundScore
+        FROM
+            `{finance_table_id}` as A
+    
+        JOIN `{twitter_table_id}` as B ON A.Ticker = B.Ticker AND A.Date = B.Date
+    """
+
+    query_job = client.query(query)
+    if query_job.state == 'DONE':
+        if query_job.error_result:
+            print(f"Query job failed with error: {query_job.error_result['message']}")
+        else:
+            print("Query job succeeded!")
+    else:
+            print("Query job is still running.")
+
+def send_email():
+    EMAIL = 'is3107group9@gmail.com'
+    PASSWORD = 'is3107group9!!'
+    '''
+    Sends an email to the recipient if the daily pipeline has been executed successfully. 
+
+    Input: email and password 
+    Output: None
+    '''
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(EMAIL,PASSWORD)
+    server.sendmail(EMAIL,EMAIL,'The daily pipeline has been executed successfully.')
+
+
+default_args = {
+     'owner': 'airflow',
+     'depends_on_past': False,
+     'email': ['fxing@example.com'],
+     'email_on_failure': False,
+     'email_on_retry': False,
+     'retries': 1
+    }
+
+with DAG(
+    'stocks_info_dag_combined',
+    default_args=default_args,
+    description='Collect Stock Info For Analysis',
+    catchup=False, 
+    start_date=datetime(2020, 12, 23), 
+    schedule_interval=timedelta(days=1)
+) as dag:
+    
+    financialsExtract = PythonOperator(
+        task_id='financials_extract',
+        provide_context=True,
+        python_callable=financials_extract
+    )
+
+    financialsStage = PythonOperator(
+        task_id='financials_stage',
+        provide_context=True,
+        python_callable=financials_stage
+    )
+
+    financialsTransform = PythonOperator(
+        task_id='financials_transform',
+        provide_context=True,
+        python_callable=financials_transform
+    )
+
+    financialsLoad = PythonOperator(
+        task_id='financials_load',
+        provide_context=True,
+        python_callable=financials_load
+    )
+
+    sendEmail = PythonOperator(
+        task_id='sendEmail',
+        python_callable=send_email,
+        dag=dag,  
+    )
+
+financialsExtract >> financialsStage >> financialsTransform >> financialsLoad >> sendEmail
---
+++ twitter_dag_combined.py
@@ -0,0 +1,351 @@
+from google.cloud import bigquery 
+import pandas as pd
+import yfinance as yf
+import os
+import json
+import snscrape.modules.twitter as sntwitter
+from datetime import timedelta  
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow import DAG
+import tweepy
+from datetime import datetime, timedelta
+from tweepy import OAuthHandler
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+from sqlalchemy import create_engine
+import smtplib
+
+username = "postgres"
+password = "postgres"
+host = "localhost"
+port = "5432"
+database = "is3107"
+
+# Create a SQLAlchemy engine to connect to PostgreSQL database
+engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
+
+# Change to your key
+# key = '/mnt/c/Users/hsinz/Desktop/nus/Y3S2/IS3107/Proj/privateKey.json'
+key = '/mnt/c/Users/darkk/OneDrive/NUS/Y3S2/IS3107/proj/is3107-test.json'
+def tweetdata_extract(ti):
+    #scrape 1 day of tweets up to 100 tweets based on tickers
+    tickers=[
+        ('d05.SI', 'dbs'),
+        ('SE', 'garena'),
+        ('SE', 'shopee'),
+        ('039.SI', 'ocbc'),
+        ('U11.SI', 'uob'),
+        ('Z74.SI', 'singtel'),
+        ('F34.SI', 'wilmar'),
+        ('C6L.SI', 'singapore airlines'),
+        ('GRAB', 'grab'),
+        ('G13.SI', 'genting'),
+        ('C38U.SI', 'capitaland'),
+        ('G07.SI', 'great eastern'),
+        ('C07.SI', 'jardine'),
+        ('A17U.SI', 'ascendas'),
+        ('S63.SI', 'st engineering'),
+        ('BN4.SI', 'keppel')
+    ]
+    
+    consumer_key = 'omhSwvUaRXdSh4fkq1Q4VhU36'
+    consumer_secret = 'a4uNlAvLxQ1AE48sP9bdLTDKP8IKxONdLZXff7Ue6foSliY3s5'
+    access_token = '1278982307303419908-RewTtBb6Uj2ySXfAr6G5o8NjDb8IRm'
+    access_secret = '1oY9ZCHHXWykuyctbtGKPposnfpZKIJpLQxUSC3Xylhwa'
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    client = tweepy.Client(
+        bearer_token='AAAAAAAAAAAAAAAAAAAAAGzylgEAAAAA5UESpwQdxCu2wO5e8x8A0looTk0%3DCIIzaw5kytSTDaW6zWNvs7b1MsshuYE9gyA06vy0dkNwqk07gB',
+        return_type=dict)
+
+    tickerName = []
+    author_id = []
+    texts = []
+    created_dates = []
+    tickerCode = []
+    likeCount = []
+   
+    # Get the current UTC date and time
+    utc_now = datetime.utcnow()
+
+    # Set the time to 00:00:00
+    utc_today = datetime.combine(utc_now.date(), datetime.min.time())
+
+    # Format the datetime object to a string
+    formatted_utc_today = utc_today.strftime('%Y-%m-%dT%H:%M:%SZ')
+    currentDate = formatted_utc_today[:10]
+    currentTime = utc_now.strftime("%H:%M:%S")
+    print(f'Retrieving tweets for date: {currentDate} as of time: {currentTime}')
+
+    for ticker in tickers:
+        print(ticker[1])
+        keyword = ticker[1] if ticker[1] != 'grab' else '#grab'
+
+        tweets = client.search_recent_tweets(query= keyword, max_results=100,
+            start_time = formatted_utc_today,
+            tweet_fields = ['author_id','created_at','text','source','lang','geo', 'public_metrics'],
+            user_fields = ['name','username','location','verified'],
+            expansions = ['geo.place_id', 'author_id'],
+            place_fields = ['country','country_code'])
+        try:
+            for tweet in tweets['data']:
+                if tweet['lang'] == 'en':
+                    tickerName.append(keyword if keyword != '#grab' else 'grab')
+                    texts.append(tweet['text'])
+                    created_dates.append(tweet['created_at'])
+                    author_id.append(tweet['author_id'])
+                    tickerCode.append(ticker[0])
+                    likeCount.append(tweet['public_metrics']['like_count'])
+        except:
+            tickerName.append('')
+            texts.append('')
+            created_dates.append('')
+            author_id.append('')
+            tickerCode.append(ticker[0])
+            likeCount.append(0)
+
+    df = pd.DataFrame({'Ticker_Name': tickerName,
+                       'Ticker': tickerCode,
+                        'Texts':texts, 
+                        'Likecount': likeCount,
+                        'Datetime': created_dates})
+    df = df.dropna(axis = 1)
+    twitter_data = df.to_json(orient='records')
+    ti.xcom_push(key='twitter_data', value=twitter_data)
+
+def tweetdata_upload(ti):
+    '''push 1 day of twitter data into bigquery 
+    '''
+    twitter_data = ti.xcom_pull(key='twitter_data', task_ids=['get_twitter_data'])
+    json_str = ''.join(twitter_data)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+
+    openfile=open(key)
+    jsondata=json.load(openfile)
+    openfile.close()
+
+    # Construct a BigQuery client object.
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= credentials_path
+    client = bigquery.Client()
+    
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".twitter_data.stock_info"
+    job_config = bigquery.LoadJobConfig(
+        encoding='UTF-8',
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+    )
+    df.to_sql("twitter_raw_data", con=engine, if_exists="append", index=False)
+
+    job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
+    job.result()
+    print(f'Appended {len(df)} rows of date')
+
+def get_staging_data(ti):
+    # pull data from staging area of twitter table in bigquery
+
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    openfile = open(key)
+    jsondata = json.load(openfile)
+
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".twitter_data.stock_info"
+
+    table = client.get_table(staging_table_id)
+    df = client.list_rows(table).to_dataframe()
+    twitter_staging_data = df.to_json(orient='records')
+
+    print(f'Successfully loaded twitterdata with {len(df)} rows')
+    ti.xcom_push(key = 'twitter_staging_data', value = twitter_staging_data)
+
+def clean_tweet(tweet):
+        # method for cleaning tweets used in clean_twitter_data dag
+
+        tweet = re.sub(r'http\S+', '', tweet)
+        # Remove mentions
+        tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
+        # Remove hashtags
+        tweet = re.sub(r'#', '', tweet)
+        # Remove non-alphabetic characters
+        tweet = re.sub(r'[^a-zA-Z\s]', '', tweet)
+        # Convert to lowercase
+        tweet = tweet.lower()
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        words = tweet.split()
+        filtered_words = [word for word in words if word not in stop_words]
+        return ' '.join(filtered_words)
+
+def splitScore(df):
+    df['Negative'] = df['Score'].apply(lambda x: x.get('neg'))
+    df['Neutral'] = df['Score'].apply(lambda x: x.get('neu'))
+    df['Positive'] = df['Score'].apply(lambda x: x.get('pos'))
+    df['Compound'] = df['Score'].apply(lambda x: x.get('compound'))
+    return df
+
+def weighted_avg(df):
+    weights = df['Likecount'] + 1
+    return (df['Compound'] * weights).sum() / weights.sum()
+
+def clean_twitter_data(ti):
+    # cleans the data, removing hashtags, stop words etc.
+
+    # twitter_data_str = ti.xcom_pull(key='twitter_data', task_ids=['get_twitter_data'])
+    twitter_data_str = ti.xcom_pull(key='twitter_staging_data')
+    json_str = ''.join(twitter_data_str)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+
+    df['Texts_cleaned'] = df['Texts'].apply(lambda x: clean_tweet(x))
+    twitter_data_cleaned = df.to_json(orient = 'records')
+    ti.xcom_push(key = 'twitter_data_cleaned', value = twitter_data_cleaned)
+    print('Sucessfully cleaned data')
+
+def process_twitter_data(ti):
+    # NLP on the cleaned data using sentiment analysis 
+
+    twitter_data_str = ti.xcom_pull(key='twitter_data_cleaned')
+    json_str = ''.join(twitter_data_str)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+    
+    sia = SentimentIntensityAnalyzer()
+    df['Score'] = df['Texts_cleaned'].apply(lambda x: sia.polarity_scores(x))
+    df = splitScore(df)
+    df['Date'] = df['Datetime'].apply(lambda x: x.date())
+    df.to_json(orient='records')
+
+    aggregated_data = df.groupby(['Ticker_Name', 'Ticker', 'Date'], as_index = False).apply(weighted_avg)
+    aggregated_data.rename(columns = {None: 'WeightedCompoundScore'}, inplace = True)
+
+    twitter_data_processed = df.to_json(orient = 'records')
+    aggregated_data_processed = aggregated_data.to_json(orient = 'records')
+    ti.xcom_push(key = 'twitter_data_processed', value = twitter_data_processed)
+    ti.xcom_push(key = 'aggregated_data_processed', value = aggregated_data_processed)
+
+    print('Successfully processed data')
+
+def tweet_processed_data_upload(ti):
+    #  uploads data into bigquery
+
+    twitter_data_processed = ti.xcom_pull(key='twitter_data_processed')
+    json_str = ''.join(twitter_data_processed)
+    df = pd.read_json(json_str, encoding='utf-8', orient = 'records')
+    df['Score'] = df['Score'].apply(json.dumps)
+    print(df.head(5))
+
+    aggregated_data_processed = ti.xcom_pull(key = 'aggregated_data_processed')
+    json_str_agg = ''.join(aggregated_data_processed)
+    df_aggregated = pd.read_json(json_str_agg, encoding='utf-8', orient = 'records')
+    df_aggregated['WeightedCompoundScore'] = df_aggregated['WeightedCompoundScore'].apply(json.dumps)
+    print(df_aggregated.head(5))
+
+
+    openfile=open(key)
+    jsondata=json.load(openfile)
+    openfile.close()
+
+    # Construct a BigQuery client object.
+    credentials_path = key
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    client = bigquery.Client()
+
+    project_id = jsondata['project_id']
+    staging_table_id = project_id + ".twitter_data.stock_processed"
+    job_config = bigquery.LoadJobConfig(
+        encoding='UTF-8',
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+    )
+
+    df.to_sql("twitter_processed_data", con=engine, if_exists="append", index=False)
+    df_aggregated.to_sql("twitter_aggregated_data", con=engine, if_exists="append", index=False)
+
+    job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
+    aggregated_table_id = project_id + '.twitter_data.stock_aggregated'
+    job = client.load_table_from_dataframe(df_aggregated, aggregated_table_id, job_config=job_config)
+
+    job.result()
+
+    print(f'Pushed data into big query table {staging_table_id}')
+    print(f'Pushed aggregated data into big query table {aggregated_table_id}')
+
+
+def send_email():
+    EMAIL = 'is3107group9@gmail.com'
+    PASSWORD = 'is3107group9!!'
+    '''
+    Sends an email to the recipient if the daily pipeline has been executed successfully. 
+
+    Input: email and password 
+    Output: None
+    '''
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(EMAIL,PASSWORD)
+    server.sendmail(EMAIL,EMAIL,'The daily pipeline has been executed successfully.')
+
+default_args = {
+     'owner': 'airflow',
+     'depends_on_past': False,
+     'email_on_failure': False,
+     'email_on_retry': False,
+     'retries': 1
+    }
+
+with DAG(
+    'zw_dag',
+    default_args=default_args,
+    description='Collect Twitter Info For Analysis',
+    catchup=False, 
+    start_date= datetime(2020, 12, 23), 
+    schedule_interval=timedelta(days=1)
+) as dag:
+    
+    get_twitter_data = PythonOperator(
+        task_id='get_twitter_data',
+        provide_context=True,
+        python_callable=tweetdata_extract
+    )
+
+    push_twitter_data = PythonOperator(
+        task_id='push_twitter_data',
+        provide_context=True,
+        python_callable=tweetdata_upload
+    )
+
+    get_staging_data = PythonOperator(
+        task_id='get_staging_data',
+        provide_context=True,
+        python_callable=get_staging_data
+    )
+
+    clean_twitter_data = PythonOperator(
+        task_id='clean_twitter_data',
+        provide_context=True,
+        python_callable=clean_twitter_data
+    )
+
+    process_twitter_data = PythonOperator(
+        task_id='process_twitter_data',
+        provide_context=True,
+        python_callable=process_twitter_data
+    )
+
+    push_processed_twitter_data = PythonOperator(
+        task_id='push_processed_twitter_data',
+        provide_context=True,
+        python_callable=tweet_processed_data_upload
+    )
+
+    sendEmail = PythonOperator(
+        task_id='sendEmail',
+        python_callable=send_email,
+        dag=dag,  
+    )
+
+get_twitter_data >> push_twitter_data >> get_staging_data >> clean_twitter_data >> process_twitter_data >> push_processed_twitter_data
